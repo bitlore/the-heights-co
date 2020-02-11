c3f819cefab5b772eb3fef14baf71798e4d12e56
Taylor Pokoj <pokojt@gmail.com>
2020-02-11 14:15:22 -0800

    updates to header, footer, and home

--- footer.php
+++ footer.php
@@ -1,37 +1,37 @@
 <?php
 /**
- * The template for displaying the footer. 
- *
+ * The template for displaying the footer.
  * Comtains closing divs for header.php.
- *
- * For more info: https://developer.wordpress.org/themes/basics/template-files/#template-partials
- */			
+ */
  ?>
-					
 				<footer class="footer" role="contentinfo">
-					
-					<div class="inner-footer grid-x grid-margin-x grid-padding-x">
-						
-						<div class="small-12 medium-12 large-12 cell">
-							<nav role="navigation">
-	    						<?php joints_footer_links(); ?>
-	    					</nav>
-	    				</div>
-						
-						<div class="small-12 medium-12 large-12 cell">
-							<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+
+					<div class="footer-wrap grid-x grid-padding-x">
+						<div class="footer-content row-padding display-flex justify-center align-center text-center small-12 medium-12 large-8 large-offset-2 cell">
+							<h6><?php echo get_field('footer_text', 'option'); ?></h6>
 						</div>
-					
-					</div> <!-- end #inner-footer -->
-				
-				</footer> <!-- end .footer -->
-			
+                        <div class="footer-links  row-padding small-12 medium-12 large-2 cell">
+                            <?php if(have_rows('footer_links', 'option')) { ?>
+                                <ul>
+                                <?php while(have_rows('footer_links', 'option')) { the_row(); ?>
+                                    <li>
+                                        <a class="footer-link" href="<?php echo get_sub_field('link')['url']; ?>" target="<?php echo get_sub_field('link')['target']; ?>">
+                                            <img class="footer-icon" src="<?php echo get_sub_field('icon')['url']; ?>" alt=""><?php echo get_sub_field('link')['title']; ?>
+                                        </a>
+                                    </li>
+                                <?php } ?>
+                                </ul>
+                            <?php } ?>
+                        </div>
+					</div>
+				</footer>
+
 			</div>  <!-- end .off-canvas-content -->
-					
+
 		</div> <!-- end .off-canvas-wrapper -->
-		
+
 		<?php wp_footer(); ?>
-		
+
 	</body>
-	
+
 </html> <!-- end page -->
--- template-home.php
+++ template-home.php
@@ -2,13 +2,24 @@
 /*
 Template Name: Homepage
 */
+$main_img = get_field('main_image')['url'];
+$content_title = get_field('content_title');
+$content = get_field('content');
 
 get_header(); ?>
 
 	<div class="content-wrap">
-
-		<section class="main-image bg-image grid-x no-max grid-padding-x" style="background-image: url(<?php echo $main_img; ?>);">
-		</section>
+		<?php if($main_img) { ?>
+			<section class="main-image bg-image grid-x no-max grid-padding-x" style="background-image: url(<?php echo $main_img; ?>);"></section>
+		<?php } ?>
+		<?php if($content) { ?>
+			<section class="section-padding home-content grid-x grid-padding-x">
+				<div class="cell small-12 text-center row-bottom-padding">
+					<h2><?php echo $content_title; ?></h2>
+					<?php echo $content; ?>
+				</div>
+			</section>
+		<?php  } ?>
 
 	</div> <!-- end content-wrap -->
 
