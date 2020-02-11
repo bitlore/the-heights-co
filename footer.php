8f86d86aeff6b73d01865e9b1f97680c35715b7e
Taylor Pokoj <pokojt@gmail.com>
2020-02-11 14:28:39 -0800

    footer fixes for mobile

--- footer.php
+++ footer.php
@@ -7,10 +7,10 @@
 				<footer class="footer" role="contentinfo">
 
 					<div class="footer-wrap grid-x grid-padding-x">
-						<div class="footer-content row-padding display-flex justify-center align-center text-center small-12 medium-12 large-8 large-offset-2 cell">
+						<div class="footer-content row-padding display-flex justify-center align-center text-center small-12 medium-6 medium-offset-3 large-8 large-offset-2 cell">
 							<h6><?php echo get_field('footer_text', 'option'); ?></h6>
 						</div>
-                        <div class="footer-links  row-padding small-12 medium-12 large-2 cell">
+                        <div class="footer-links row-padding small-12 medium-3 large-2 cell">
                             <?php if(have_rows('footer_links', 'option')) { ?>
                                 <ul>
                                 <?php while(have_rows('footer_links', 'option')) { the_row(); ?>
