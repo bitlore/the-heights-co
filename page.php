9f8e2377df32098ce4f0780fdb55e083c4e395b7
Taylor Pokoj <pokojt@gmail.com>
2020-03-05 12:39:36 -0800

    work for partners page

--- page.php
+++ page.php
@@ -7,21 +7,13 @@
 
 get_header(); ?>
 
-	<div class="content">
-
-		<div class="inner-content grid-x grid-margin-x grid-padding-x">
-
-		    <main class="main small-12 cell" role="main">
+	<div class="content-wrap">
 
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
 
 			    <?php endwhile; endif; ?>
-
-			</main> <!-- end #main -->
-
-		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
 
---
+++ template-gallery.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Gallery
+*/
+
+
+get_header(); ?>
+
+	<div class="content-wrap">
+
+
+	</div> <!-- end content-wrap -->
+
+<?php get_footer(); ?>
