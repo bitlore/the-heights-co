32d5badf1f566a6107bc1973719dc579942fe95d
Taylor Pokoj <pokojt@gmail.com>
2020-05-17 22:48:56 -0700

    testing with sample request

--- template-order.php
+++ template-order.php
@@ -10,10 +10,14 @@ get_header(); ?>
 	<section class="grid-x grid-padding-x section-padding">
 		<div class="cell small-12">
 
-			<div class="form-wrapper">
+			<div class="form-wrapper section-bottom-padding">
+				<h1>ORDER FORM</h1>
 				<?php gravity_form( 'Order', true, false, false, $ajax = true ); ?>
 			</div>
-
+			<div class="form-wrapper">
+				<h1>SAMPLE REQUEST</h1>
+				<?php gravity_form( 'Sample Request', true, false, false, $ajax = true ); ?>
+			</div>
 		</div>
 	</section>
 
